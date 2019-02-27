41fe1447fb0518f02ffe5b241bc1da3ce5b5c13b
Rinbo <robin.b@outlook.com>
2019-02-27 20:43:00 +0100

    Add validate user to cdm in app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 import Authpage from "./Authpage";
+import endpoint from "./endpoint";
 
 export class App extends Component {
+  componentDidMount = () => {
+    const token = localStorage.getItem("token");
+    endpoint
+      .post("/users/validatetoken", { token })
+      .then(response => {
+        localStorage.setItem("token", response.data.token);
+        this.setState({
+          isSignedIn: true,
+          signedInUser: response.data.name,
+          userEmail: response.data.email
+        });
+        console.log(response.data.token);
+      })
+      .catch(e => console.log(e));
+  };
+
   state = {
-    isSignedIn: null
+    isSignedIn: null,
+    signedInUser: null,
+    userEmail: null
   };
 
   render() {
