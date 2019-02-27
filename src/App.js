d96e6a80aae3103152220ffb6294ee5937f1a876
Rinbo <robin.b@outlook.com>
2019-02-27 20:56:49 +0100

    Add spinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import Authpage from "./Authpage";
 import endpoint from "./endpoint";
+import Spinner from "./Spinner";
 
 export class App extends Component {
+  state = {
+    isSignedIn: null,
+    signedInUser: null,
+    userEmail: null,
+    isLoading: true
+  };
+
   componentDidMount = () => {
     const token = localStorage.getItem("token");
     endpoint
@@ -12,25 +20,25 @@ export class App extends Component {
         this.setState({
           isSignedIn: true,
           signedInUser: response.data.name,
-          userEmail: response.data.email
+          userEmail: response.data.email,
+          isLoading: false
         });
         console.log(response.data.token);
       })
-      .catch(e => console.log(e));
-  };
-
-  state = {
-    isSignedIn: null,
-    signedInUser: null,
-    userEmail: null
+      .catch(e => {
+        this.setState({ isLoading: false });
+        console.log("Failed to validate token");
+      });
   };
 
   render() {
-    const { isSignedIn } = this.state;
+    const { isSignedIn, isLoading } = this.state;
+    if (isLoading) {
+      return <Spinner />;
+    }
     if (!isSignedIn) {
       return <Authpage auth={this.auth} />;
     }
-
     return <div>Welcome!</div>;
   }
 }
