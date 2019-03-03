7a9d40713881da56577a0b8d1e9e7343bd54361d
Rinbo <robin.b@outlook.com>
2019-03-03 22:38:34 +0100

    Scaffold game components

--- src/components/game/GameContainer.js
+++ src/components/game/GameContainer.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 export class GameContainer extends Component {
+  state = {
+    code: "",
+    indicator: "grey"
+  };
+
   renderJSX = () => {
     const jsx = `import {setHeader} from "../apis/axios"`;
     return jsx;
@@ -12,6 +17,24 @@ export class GameContainer extends Component {
         <pre>
           <code>{this.renderJSX()}</code>
         </pre>
+        <div
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: this.state.indicator,
+            margin: "auto"
+          }}
+        />
+        <form className="ui form">
+          <div className="field">
+            <input
+              type="text"
+              onChange={this.parseInput}
+              value={this.state.code}
+              style={{ width: 400, marginTop: 30 }}
+            />
+          </div>
+        </form>
       </div>
     );
   }
