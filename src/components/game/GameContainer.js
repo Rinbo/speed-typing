c6e664db2960f8c4a3f59cceadf7b4d3b2932e75
Rinbo <robin.b@outlook.com>
2019-03-04 00:04:41 +0100

    Add some game logic

--- src/components/game/GameContainer.js
+++ src/components/game/GameContainer.js
@@ -2,35 +2,61 @@ import React, { Component } from "react";
 
 export class GameContainer extends Component {
   state = {
-    code: "",
-    indicator: "grey"
+    typedCode: "",
+    displayCode: `import {setHeader} from "../apis/axios"`,
+    colorArray: [],
+    keyUsed: null
   };
 
-  renderJSX = () => {
-    const jsx = `import {setHeader} from "../apis/axios"`;
-    return jsx;
+  renderCode = () => {
+    const { displayCode, colorArray } = this.state;
+    return displayCode.split("").map((char, index) => {
+      return (
+        <code
+          key={index}
+          style={{ color: colorArray[index] ? colorArray[index] : "black" }}
+        >
+          {char}
+        </code>
+      );
+    });
+  };
+
+  parseInput = e => {
+    e.preventDefault();
+    const { displayCode, keyUsed } = this.state;
+    const index = e.target.value.length - 1;
+    this.setState({ typedCode: e.target.value });
+    const colorArray = { ...this.state.colorArray };
+    console.log(keyUsed);
+    if (keyUsed === "Backspace") {
+      colorArray[index + 1] = "black";
+      this.setState({ colorArray: colorArray });
+    }
+
+    if (e.target.value[index] === displayCode[index]) {
+      colorArray[index] = "lightgreen";
+      this.setState({ colorArray: colorArray });
+    } else {
+      colorArray[index] = "red";
+      this.setState({ colorArray: colorArray });
+    }
   };
 
   render() {
     return (
       <div style={{ textAlign: "center", marginTop: "10vh" }}>
         <pre>
-          <code>{this.renderJSX()}</code>
+          <code>{this.renderCode()}</code>
         </pre>
-        <div
-          style={{
-            width: 50,
-            height: 50,
-            backgroundColor: this.state.indicator,
-            margin: "auto"
-          }}
-        />
         <form className="ui form">
           <div className="field">
             <input
               type="text"
+              spellCheck="false"
               onChange={this.parseInput}
-              value={this.state.code}
+              onKeyDown={e => this.setState({ keyUsed: e.key })}
+              value={this.state.typedCode}
               style={{ width: 400, marginTop: 30 }}
             />
           </div>
