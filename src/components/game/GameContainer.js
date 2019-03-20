c5d512a94e342930809878d13c485155c7428400
Rinbo <robin.b@outlook.com>
2019-03-20 22:38:39 +0100

    Give up on functional component for GameContainer => Move to an update function passed as props to it instead

--- src/components/game/GameContainer.js
+++ src/components/game/GameContainer.js
@@ -1,96 +1,11 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { Component } from "react";
 import GameField from "./GameField";
 import file from "../resources/game.txt";
 import GameComplete from "./GameComplete";
 import endpoint from "../apis/endpoint";
 import { setHeaders } from "../apis/setHeaders";
 
-export const GameContainer = () => {
-  const [typedCode, setTypedCode] = useState("");
-  const [displayCode, setDisplayCode] = useState("");
-  const [score, setScore] = useState("");
-  const [gameStatus, setGameStatus] = useState("");
-  const [codeRepo, setCodeRepo] = useState("");
-
-  useEffect(() => {
-    fetch(file)
-      .then(response => response.text())
-      .then(text => {
-        const lines = text.split("\n");
-        setCodeRepo(lines);
-        getRandomCode(lines);        
-      });
-  }, []);
-
-  const getRandomCode = (codeLines = null) => {
-    //codeLines = codeLines === null ? codeRepo : codeLines;
-    const randomNumber = Math.floor(Math.random() * codeLines.length);
-    setDisplayCode(codeLines[randomNumber].trim());
-  };
-
-  const restart = () => {
-    setGameStatus("ready");
-    setTypedCode("");
-    setScore(0);
-  };
-
-  const parseInput = input => {
-    if (input === displayCode) {
-      const currentScore = score;
-      setTypedCode("");
-      setScore(currentScore + displayCode.length);
-      getRandomCode();
-    } else {
-      setTypedCode(input);
-    }
-  };
-
-  const gameComplete = () => {
-    const currentScore = score;
-    const remainingScore = countRemaingScore();
-    setScore(currentScore + remainingScore);
-    setGameStatus("complete");
-    updateHighScore();
-  };
-
-  const updateHighScore = () => {
-    setHeaders();
-    endpoint.put("/highscores/update", { score }).then(response => {
-      localStorage.setItem("token", response.headers.token);
-      alert(response.data);
-    });
-  };
-
-  const countRemaingScore = () => {
-    let counter = 0;
-    displayCode.split("").forEach((char, index) => {
-      if (char === typedCode[index]) {
-        counter += 1;
-      }
-    });
-    return counter;
-  };
-
-  if (gameStatus === "ready") {
-    return (
-      <GameField
-        typedCode={typedCode}
-        displayCode={displayCode}
-        parseInput={parseInput}
-        gameComplete={gameComplete}
-        restart={restart}
-      />
-    );
-  }
-  if (gameStatus === "complete") {
-    return <GameComplete restart={restart} score={score} />;
-  }
-  return null;
-};
-
-export default GameContainer;
-
-/* export class GameContainer extends Component {
+export class GameContainer extends Component {
   state = {
     typedCode: "",
     displayCode: ``,
@@ -185,4 +100,4 @@ export default GameContainer;
   }
 }
 
-export default GameContainer; */
+export default GameContainer;
