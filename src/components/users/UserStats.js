c2ad097addf5691f5c5d6eec2d50e62e21057d34
Rinbo <robin.b@outlook.com>
2019-04-14 21:47:03 +0200

    Switch to using chartjs instead of canvasjs

--- src/components/users/UserStats.js
+++ src/components/users/UserStats.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
-
-import CanvasJSReact from "../../assets/canvasjs.react";
+import { Bar, defaults } from "react-chartjs-2";
 
 const XAXIS_PADDING = 10;
 const GRANULARITY = 5;
 
 const UserStats = ({ state }) => {
   const [dataPoints, setDataPoints] = useState([]);
-  const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+  const [labels, setLabels] = useState([]);
+  const [data, setData] = useState([]);
+  defaults.global.defaultFontColor = "#cccccc";
 
   useEffect(() => {
     calculateDataPoints();
@@ -34,34 +35,76 @@ const UserStats = ({ state }) => {
       incrementCount++;
     }
     setDataPoints(histogram);
+    setLabels(histogram.map(obj => `${obj.x} - ${obj.x + GRANULARITY} `));
+    setData(histogram.map(obj => obj.y));
   };
 
   const options = {
-    animationEnabled: true,
-    theme: "dark2",
-    backgroundColor: "transparent",
-    axisY: {
-      title: "# of Results"
+    legend: {
+      display: false
     },
-    axisX: {
-      title: "Score"
+    scales: {
+      xAxes: [
+        {
+          display: true,
+          gridLines: {
+            display: false,
+            color: "#cccccc"
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Score"
+          }
+        }
+      ],
+      yAxes: [
+        {
+          display: true,
+          gridLines: {
+            display: true,
+            color: "rgba(204, 204, 204, 0.5)"
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Frequency"
+          }
+        }
+      ]
     },
-    data: [
-      {
-        type: "column",
-        dataPoints: dataPoints
-      }
-    ]
+    responsive: true,
+    maintainAspectRatio: false
   };
+
   if (dataPoints.length === 0) return null;
+
   return (
-    <div>
-      <CanvasJSChart options={options} />
-      <p>
-        Histogram of your result history. Your scores are grouped in intervals
+    <div style={{ marginTop: 50 }} className="chart-container">
+      <div className="ui inverted centered sub header">Histogram</div>
+      <p style={{ fontSize: 10, marginTop: 20 }}>
+        Histogram of your result history - Your scores are grouped in intervals
         of 5 on the x-axis and the number of results in each interval is shown
         on the y-axis
       </p>
+      <Bar
+        data={{
+          labels,
+          datasets: [
+            {
+              label: "Number of results in score range",
+              backgroundColor: "rgba(33,186,69,0.2)",
+              borderColor: "rgba(33,186,69,1)",
+              borderWidth: 1,
+              defaultFontColor: "#cccccc",
+              hoverBackgroundColor: "rgba(33,186,69,0.4)",
+              hoverBorderColor: "rgba(33,186,69,1)",
+              data
+            }
+          ]
+        }}
+        width={400}
+        height={400}
+        options={options}
+      />
     </div>
   );
 };
