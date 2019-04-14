7767d7bccf3ea00d42926763cf97bbbfb8a89a10
Rinbo <robin.b@outlook.com>
2019-04-14 18:56:29 +0200

    Add CanvasJS

--- src/components/users/UserStats.js
+++ src/components/users/UserStats.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 
+var CanvasJSReact = require("../../assets/canvasjs.react");
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const XAXIS_PADDING = 10;
+const GRANULARITY = 5;
+
 const UserStats = ({ state }) => {
   const [dataPoints, setDataPoints] = useState([]);
 
@@ -8,25 +15,42 @@ const UserStats = ({ state }) => {
   }, []);
 
   const calculateDataPoints = () => {
-    const scores = state.userScores;
-    const prunedScores = scores.filter(score => score.score !== 0).reverse();
-    const granularity = 5;
+    const scores = state.userScores.map(score => score.score);
+    const prunedScores = scores.filter(score => score !== 0).reverse();
     const histogram = [];
+    const min = Math.min.apply(null, prunedScores);
+    const max = Math.max.apply(null, prunedScores);
+    const minX = min - (min % XAXIS_PADDING);
+    const maxX = max + (XAXIS_PADDING - (max % XAXIS_PADDING));
+    let incrementCount = 0;
     let yValues = [];
-    for (let i = 0; i < prunedScores.length; i) {
-      // Continue here - not working properly below
+    for (let i = minX; i <= maxX; i += GRANULARITY) {
       yValues = prunedScores.filter(
-        score =>
-          score >= prunedScores[i].score &&
-          score < prunedScores[i].score + granularity
+        score => score >= i && score < i + GRANULARITY
       );
-      histogram[i] = { x: prunedScores[i].score, y: yValues.length };
+      histogram[incrementCount] = {
+        x: i,
+        y: yValues.length
+      };
+      incrementCount++;
     }
     setDataPoints(histogram);
   };
-  console.log(dataPoints);
 
-  return <div />;
+  const options = {
+    title: {
+      text: "Histogram of your score history"
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: dataPoints
+      }
+    ]
+  };
+
+  if (dataPoints.length === 0) return null;
+  return <CanvasJSChart options={options} />;
 };
 
 export default UserStats;
