8f0c5d846f867cf75a87696f0f78e92ac47e19fa
Rinbo <robin.b@outlook.com>
2020-07-04 11:11:49 +0200

    Change dns

--- src/components/apis/endpoint.js
+++ src/components/apis/endpoint.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://localhost:5000",
-  baseURL: "https://typing2.borjessons.nu/game-backend",
-  //baseURL: "http://ec2-13-53-130-252.eu-north-1.compute.amazonaws.com:8080/game-backend",
+  baseURL: "https://api.borjessons.nu/game-backend",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, Content-Type, Accept",
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
